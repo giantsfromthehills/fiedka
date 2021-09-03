818ec34631f59f8282c00e45026871fd6f5e28a9
turretd <turretd@local>
2021-09-03 21:32:14 +0200

    take source for website from niv

--- nix/default.nix
+++ nix/default.nix
@@ -1,17 +1,13 @@
 let 
   sources = import ./sources.nix;
   pkgs = import sources.nixpkgs {};
+  src = import sources.website;
 in
 with pkgs;
 {
   website = stdenv.mkDerivation {
   name = "fiedka-website";
-  src = fetchFromGitHub {
-      owner = "fiedka";
-      repo = "website";
-      rev = "3c13a6e03008ab1570e6cfd77e1c0aed95c2d4dd";
-      sha256 = "0nhkx3238fbjawi48x2k64gyrbay36f8cw4pl2ix54ci5ks0amf9";
-  };
+  src = src;
   # Add the derivation to the PATH
   installPhase = ''
   mkdir -p $out
