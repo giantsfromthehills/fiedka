f80271a5e0e049c27ac23837c60d1f8eadeaec17
turretd <turretd@local>
2021-09-03 19:59:44 +0200

    setup github upgrade functionality

---
+++ nix/default.nix
@@ -0,0 +1,21 @@
+let 
+  sources = import ./sources.nix;
+  pkgs = import sources.nixpkgs {};
+in
+with pkgs;
+{
+  website = stdenv.mkDerivation {
+  name = "fiedka-website";
+  src = fetchFromGitHub {
+      owner = "fiedka";
+      repo = "website";
+      rev = "3c13a6e03008ab1570e6cfd77e1c0aed95c2d4dd";
+      sha256 = "0nhkx3238fbjawi48x2k64gyrbay36f8cw4pl2ix54ci5ks0amf9";
+  };
+  # Add the derivation to the PATH
+  installPhase = ''
+  mkdir -p $out
+  cp index.html $out/
+  '';
+};
+}
